daf2a8c0dfec278269dd7741922db1de5102d80c
Luis Sigcha <38364094+luisfrancs@users.noreply.github.com>
2026-02-04 17:24:15 +0100

    Add files via upload

--- models/pretrained_models.py
+++ models/pretrained_models.py
@@ -2,24 +2,16 @@
 ## AUTHOR Luis Sigcha
 
 from tensorflow.keras.models import model_from_json
+from pathlib import Path
 
-def multi_taks_7classes():
-    # Load model
-    with open("MODEL_nuWIDECNN_AP_AG_macmetal_2025-11-20_14-31-31_f1score_0.761_model_architecture.json", "r") as json_file:
-        loaded_model = model_from_json(json_file.read())
-    # Load weigths
-    loaded_model.load_weights('MODEL_nuWIDECNN_AP_AG_macmetal_2025-11-20_14-31-31_f1score_0.761.weights.h5')
-    encoding_dict_PB = {0:'Sitting', 1:'Standing', 2:'Walking', 3:'Running', 4:'Sports', 5:'Cycling', 6:'Lying'}
-    encoding_dict_EE = {0:'Sedentary', 1:'LPA', 2:'MVPA'}
-    return loaded_model, encoding_dict_PB, encoding_dict_EE
+_THIS_DIR = Path(__file__).resolve().parent #build relative paths
 
 def multi_taks_7classes_V2():
     # Load model (V2 vesion has a better EE fscore)
-    with open(
-        "MODEL_nuWIDECNN_AP_AG_macmetal_2025-11-26_19-19-37_f1score_0.76_betterEE_model_architecture.json", "r") as json_file:
+    with open(_THIS_DIR /"MODEL_nuWIDECNN_AP_AG_macmetal_2025-11-26_19-19-37_f1score_0.76_betterEE_model_architecture.json", "r") as json_file:
         loaded_model = model_from_json(json_file.read())
     # Load weigths
-    loaded_model.load_weights('MODEL_nuWIDECNN_AP_AG_macmetal_2025-11-26_19-19-37_f1score_0.76_betterEE.weights.h5')
+    loaded_model.load_weights(_THIS_DIR /'MODEL_nuWIDECNN_AP_AG_macmetal_2025-11-26_19-19-37_f1score_0.76_betterEE.weights.h5')
     encoding_dict_PB = {0:'Sitting', 1:'Standing', 2:'Walking', 3:'Running', 4:'Sports', 5:'Cycling', 6:'Lying'}
     encoding_dict_EE = {0:'Sedentary', 1:'LPA', 2:'MVPA'}
     return loaded_model, encoding_dict_PB, encoding_dict_EE
@@ -27,10 +19,10 @@ def multi_taks_7classes_V2():
 def multi_taks_7classes_AP():
     # Load model
     with open(
-        "MODEL_nuWIDECNN_AP_SINGLE_macmetal_2025-11-21_15-29-12_f1score_0.728_model_architecture.json", "r") as json_file:
+        _THIS_DIR /"MODEL_nuWIDECNN_AP_SINGLE_macmetal_2025-11-21_15-29-12_f1score_0.728_model_architecture.json", "r") as json_file:
         loaded_model = model_from_json(json_file.read())
     # Load weigths
-    loaded_model.load_weights('MODEL_nuWIDECNN_AP_SINGLE_macmetal_2025-11-21_15-29-12_f1score_0.728.weights.h5')
+    loaded_model.load_weights(_THIS_DIR /'MODEL_nuWIDECNN_AP_SINGLE_macmetal_2025-11-21_15-29-12_f1score_0.728.weights.h5')
     encoding_dict_PB = {0:'Sitting', 1:'Standing', 2:'Walking', 3:'Running', 4:'Sports', 5:'Cycling', 6:'Lying'}
     encoding_dict_EE = {0:'Sedentary', 1:'LPA', 2:'MVPA'}
     return loaded_model, encoding_dict_PB, encoding_dict_EE
@@ -38,10 +30,10 @@ def multi_taks_7classes_AP():
 def multi_taks_7classes_AG():
     # Load model
     with open(
-        "MODEL_nuWIDECNN_AG_SINGLE_macmetal_2025-12-03_14-26-02_f1score_0.596_model_architecture.json", "r") as json_file:
+        _THIS_DIR /"MODEL_nuWIDECNN_AG_SINGLE_macmetal_2025-12-03_14-26-02_f1score_0.596_model_architecture.json", "r") as json_file:
         loaded_model = model_from_json(json_file.read())
     # Load weigths
-    loaded_model.load_weights('MODEL_nuWIDECNN_AG_SINGLE_macmetal_2025-12-03_14-26-02_f1score_0.596.weights.h5')
+    loaded_model.load_weights(_THIS_DIR /'MODEL_nuWIDECNN_AG_SINGLE_macmetal_2025-12-03_14-26-02_f1score_0.596.weights.h5')
     encoding_dict_PB = {0:'Sitting', 1:'Standing', 2:'Walking', 3:'Running', 4:'Sports', 5:'Cycling', 6:'Lying'}
     encoding_dict_EE = {0:'Sedentary', 1:'LPA', 2:'MVPA'}
     return loaded_model, encoding_dict_PB, encoding_dict_EE
