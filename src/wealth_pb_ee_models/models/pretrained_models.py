a01010d12f9a6d6a9051353ed501be476b243087
Luis Sigcha <38364094+luisfrancs@users.noreply.github.com>
2026-02-10 09:40:26 +0100

    Update pretrained_models.py

--- src/wealth_pb_ee_models/models/pretrained_models.py
+++ src/wealth_pb_ee_models/models/pretrained_models.py
@@ -1,7 +1,8 @@
-## VERSION 3 DECEMBER 2025
+## VERSION 10 February 2026
 ## AUTHOR Luis Sigcha
 
-from tensorflow.keras.models import model_from_json
+import tensorflow as tf
+#from tensorflow.keras.models import model_from_json
 from pathlib import Path
 
 _THIS_DIR = Path(__file__).resolve().parent #build relative paths
@@ -16,13 +17,20 @@ def multi_taks_7classes_V2():
     encoding_dict_EE = {0:'Sedentary', 1:'LPA', 2:'MVPA'}
     return loaded_model, encoding_dict_PB, encoding_dict_EE
 
-def multi_taks_7classes_AP():
+def multi_taks_7classes_APV1():
     # Load model
     with open(
         _THIS_DIR /"MODEL_nuWIDECNN_AP_SINGLE_macmetal_2025-11-21_15-29-12_f1score_0.728_model_architecture.json", "r") as json_file:
         loaded_model = model_from_json(json_file.read())
     # Load weigths
     loaded_model.load_weights(_THIS_DIR /'MODEL_nuWIDECNN_AP_SINGLE_macmetal_2025-11-21_15-29-12_f1score_0.728.weights.h5')
+    encoding_dict_PB = {0:'Sitting', 1:'Standing', 2:'Walking', 3:'Running', 4:'Sports', 5:'Cycling', 6:'Lying'}
+    encoding_dict_EE = {0:'Sedentary', 1:'LPA', 2:'MVPA'}
+    return loaded_model, encoding_dict_PB, encoding_dict_EE
+
+def multi_taks_7classes_AP():
+    # Load model
+    loaded_model = tf.keras.models.load_model(_THIS_DIR /'MODEL_nuWIDECNN_AP_SINGLE_2025-11-21_15-29-12_f1score_0.728_updated.h5')
     encoding_dict_PB = {0:'Sitting', 1:'Standing', 2:'Walking', 3:'Running', 4:'Sports', 5:'Cycling', 6:'Lying'}
     encoding_dict_EE = {0:'Sedentary', 1:'LPA', 2:'MVPA'}
     return loaded_model, encoding_dict_PB, encoding_dict_EE
@@ -37,4 +45,3 @@ def multi_taks_7classes_AG():
     encoding_dict_PB = {0:'Sitting', 1:'Standing', 2:'Walking', 3:'Running', 4:'Sports', 5:'Cycling', 6:'Lying'}
     encoding_dict_EE = {0:'Sedentary', 1:'LPA', 2:'MVPA'}
     return loaded_model, encoding_dict_PB, encoding_dict_EE
-
